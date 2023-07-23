469b131341952f06a2f493063f4de692f42f2324
marius <marius1890@yahoo.com>
2023-07-23 20:57:46 +0300

    update

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import requests
 
 
-lon = 23.58
-lat = 46.76
+# lon = 23.58
+# lat = 46.76
 parametrii: dict = {
     'q': 'Cluj-Napoca',
-    'ro&APPID': 'doar nu sunt bou sa ti-l arat!'
+    'appid': '****'
 }
-raspuns = requests.get(url='api.openweathermap.org/data/2.5/weather?', params=parametrii)
+raspuns = requests.get(url='http://api.openweathermap.org/data/2.5/weather?', params=parametrii)
 raspuns.raise_for_status()
 date_json: dict = raspuns.json()
 print(date_json)
