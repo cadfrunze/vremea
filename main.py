43f57689a37bcbb847c3c3d71e771bf6eb6e0183
marius <marius1890@yahoo.com>
2023-08-09 22:12:07 +0300

    update

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ ALERT_VREMEA: list = [1006, 1009, 1063, 1087, 1180, 1183, 1186, 1189, 1192, 1195
                       1273, 1276]
 
 MY_API = '37f43c11b8f14ec799371509232907'
-ora_actuala: int = datetime.now().hour
+
 parametrii: dict = {
     'key': MY_API,
-    'q': 'Salzburg',
+    'q': 'Kurye',
     # 'hour': ora_actuala,
     'lang': 'ro'
 
@@ -18,8 +18,7 @@ parametrii: dict = {
 
 def vremea():
     """Functie pt avertizare vreme rea!"""
-    global ora_actuala
-    if ora_actuala == 9 or ora_actuala == 21:
+    if datetime.now().hour == 9 or datetime.now().hour == 22:
         while True:
             try:
                 raspuns = requests.get(url='http://api.weatherapi.com/v1/forecast.json', params=parametrii)
@@ -31,26 +30,25 @@ def vremea():
                 # raspuns va primi valoarea de counexiune 200 si va prelua datele
                 date_json: dict = raspuns.json()
                 # print(date_json)
-                rezultat: list = date_json['forecast']['forecastday'][0]['hour'][ora_actuala:ora_actuala + 4]
+                rezultat: list = date_json['forecast']['forecastday'][0]['hour'][datetime.now().hour:datetime.now().hour + 4]
                 coduri_target: dict = {}
                 for element in rezultat:
                     if element['condition']['code'] in ALERT_VREMEA:
                         new_element: str = element['time'].split(' ')[-1]
                         coduri_target[element['condition']['code']] = new_element
                 if len(coduri_target) > 1:
-                    mesaj: str = 'Intre orele: ' + ','.join(coduri_target.values()) + ' trebuie sa iti iei umbrela!'
-                    print(mesaj)
-                else:
-                    mesaj: str = 'La ora ' + ','.join(coduri_target.values()) + ' trebuie sa iti iei umbrela!'
-                    print(mesaj)
+                    if len(coduri_target) > 1:
+                        mesaj: str = 'Intre orele: ' + ','.join(coduri_target.values()) + ' trebuie sa iti iei umbrela!'
+                        print(mesaj)
+                    else:
+                        mesaj: str = 'La ora ' + ','.join(coduri_target.values()) + ' trebuie sa iti iei umbrela!'
+                        print(mesaj)
                 minut_sleep = 60 - datetime.now().minute
                 # print(minut_sleep)
                 print(f"Trebuie sa ateptam: {(minut_sleep + 1) * 60} secunde")
                 time.sleep(int((minut_sleep + 1) * 60))
                 break
-    ora_actuala = datetime.now().hour
-    while ora_actuala != 9 and ora_actuala != 0:
-        ora_actuala = datetime.now().hour
+    while datetime.now().hour != 9 and datetime.now().hour != 0:
         print('am intrat in ora diferita')
         # Daca ora este diferita va continua pana la ora afisata conform cerintelor
         continue
