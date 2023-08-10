d6d2982b1adb7185816315ed47273f451d167c79
cadfrunze <marius1890@yahoo.com>
2023-08-10 21:35:41 +0300

    update

--- main.py
+++ main.py
@@ -9,16 +9,15 @@ MY_API = '37f43c11b8f14ec799371509232907'
 
 parametrii: dict = {
     'key': MY_API,
-    'q': 'Kurye',
+    'q': 'Veliki Preslav',
     # 'hour': ora_actuala,
     'lang': 'ro'
 
 }
 
 
-def vremea():
-    """Functie pt avertizare vreme rea!"""
-    if datetime.now().hour == 9 or datetime.now().hour == 22:
+while True:
+    if datetime.now().hour == 9 or datetime.now().hour == 21:
         while True:
             try:
                 raspuns = requests.get(url='http://api.weatherapi.com/v1/forecast.json', params=parametrii)
@@ -30,7 +29,7 @@ def vremea():
                 # raspuns va primi valoarea de counexiune 200 si va prelua datele
                 date_json: dict = raspuns.json()
                 # print(date_json)
-                rezultat: list = date_json['forecast']['forecastday'][0]['hour'][datetime.now().hour:datetime.now().hour + 4]
+                rezultat: list = date_json['forecast']['forecastday'][0]['hour'][datetime.now().hour:datetime.now().hour+4]
                 coduri_target: dict = {}
                 for element in rezultat:
                     if element['condition']['code'] in ALERT_VREMEA:
@@ -43,16 +42,12 @@ def vremea():
                     else:
                         mesaj: str = 'La ora ' + ','.join(coduri_target.values()) + ' trebuie sa iti iei umbrela!'
                         print(mesaj)
+                    coduri_target.clear()
                 minut_sleep = 60 - datetime.now().minute
                 # print(minut_sleep)
                 print(f"Trebuie sa ateptam: {(minut_sleep + 1) * 60} secunde")
                 time.sleep(int((minut_sleep + 1) * 60))
-                break
     while datetime.now().hour != 9 and datetime.now().hour != 0:
         print('am intrat in ora diferita')
         # Daca ora este diferita va continua pana la ora afisata conform cerintelor
         continue
-    vremea()
-
-
-vremea()
